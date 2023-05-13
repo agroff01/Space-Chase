d19147cae44a32e207fac8f83f1c5dcb055ba190
Alex Groff <amgroff@ucsc.edu>
2023-05-13 16:51:58 -0700

    Final Comments in Main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-
+// Name: Alex Groff
+// Game: Space Chase
+// Approx Hours: 16 hours
+// Creative Tilt:
+//         -Technical: To show that the player has been hit and is temporarily invulrable, I have the sprite's alpha value rapidly fluctuate on every game update. Simple, but effective! 
+//                     I also made the metors randomly choose their own sprite and color from an array every time one was spawned.
+//         -Visual Style: Im particularly proud of the animations for the police cruzers and their lazer shots at the player. Animation is something I've never really tried to do before.
 
 let config = {
     type: Phaser.AUTO,
